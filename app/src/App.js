bb7af37e7ae206fd2bbf412cca148d4418b9db3b
Matthew Behan-Fossey <matthewbf8@gmail.com>
2020-12-17 16:48:38 -0700

    Added styling to editor page

--- app/src/App.js
+++ app/src/App.js
@@ -5,8 +5,10 @@ import TextEditor from './TextEditor/TextEditor.js'
 
 function App() {
   return (
-    <div>
-      <TextEditor />
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center',}}>
+      <div style={{border: '1px solid rgba(196, 196, 196, 0.5)', minHeight: 400, width: 500, margin: 20, padding: 10}}>
+        <TextEditor />
+      </div>
     </div>
   );
 }
