a8e3019d96c98b30722b1897837ba72547b81aa1
Matthew Behan-Fossey <matthewbf8@gmail.com>
2020-12-20 18:37:18 -0700

    Extracted editor functions

--- app/src/TextEditor/TextEditor.js
+++ app/src/TextEditor/TextEditor.js
@@ -11,9 +11,48 @@ import { Slate, Editable, withReact } from 'slate-react'
 import { CodeBlock } from './CustomElements/CustomElements.js'
 import { Leaf } from './CustomLeaves/CustomLeaves.js'
 
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true,
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+}
+
+
+
 const TextEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
   const [value, setValue] = useState([
     {
       type: 'paragraph',
@@ -21,20 +60,10 @@ const TextEditor = () => {
     },
   ])
 
-  useEffect(() => {
-    if (!value) {
-       //Transforms.deselect(editor);
-       // or set selection to neutral state:
-       editor.selection = { anchor: { path: [0,0], offset:0 }, focus: { path: [0,0], offset: 0 } }
-       // Transforms.select(editor, {
-       //   anchor: { path: [0, 0], offset: 0 },
-       //   focus: { path: [0, 0], offset: 0 },
-       // })
-
-    }
-  }, [value]);
-
   //FOR RENDERING CUSTOM BLOCKS
+  const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>
+  }
   const renderElement = useCallback(props => {
     switch (props.element.type) {
       case 'code':
@@ -44,68 +73,160 @@ const TextEditor = () => {
     }
   }, [])
 
-  //FOR RENDERING CUSTOM LEAVES
   const renderLeaf = useCallback(props => {
     return <Leaf {...props} />
   }, [])
 
-  const DefaultElement = props => {
-    return <p {...props.attributes}>{props.children}</p>
-  }
-
   return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={newValue => setValue(newValue)}
-
-      style={{border: 'solid', width: '100%', height: '100%'}}
-    >
-    <Editable
-      renderElement={renderElement}
-      renderLeaf={renderLeaf}
-
-      onKeyDown={event => {
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
           let commandKey = event.ctrlKey || event.metaKey ? true : false
           if (!commandKey) {
             return
-          } else {
-            console.log("command issued " + event.key)
-            switch (event.key) {
-              // When "`" is pressed, keep our existing code block logic.
-              case '`': {
-                event.preventDefault()
-                const [match] = Editor.nodes(editor, {
-                  match: n => n.type === 'code',
-                })
-                Transforms.setNodes(
-                  editor,
-                  { type: match ? 'paragraph' : 'code' },
-                  { match: n => Editor.isBlock(editor, n) }
-                )
-                break
-              }
+          }
 
-              // When "B" is pressed, bold the text in the selection.
-              case 'b': {
-                console.log("b pressed")
-                // Apply it to text nodes, and split the text node up if the
-                // selection is overlapping only part of it.
-                event.preventDefault()
-                Transforms.setNodes(
-                  editor,
-                  { bold: !editor.bold },
-                  { match: n => Text.isText(n), split: true }
-                )
-                break
-              }
+          // Replace the `onKeyDown` logic with our new commands.
+          switch (event.key) {
+            case '`': {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+              break
+            }
+
+            case 'b': {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+              break
             }
           }
         }}
-
-    />
+      />
     </Slate>
   )
 }
+
+//
+// // Define our own custom set of helpers.
+// const CustomEditor = {
+//   isBoldMarkActive(editor) {
+//     const [match] = Editor.nodes(editor, {
+//       match: n => n.bold === true,
+//       universal: true,
+//     })
+//
+//     return !!match
+//   },
+//
+//   isCodeBlockActive(editor) {
+//     const [match] = Editor.nodes(editor, {
+//       match: n => n.type === 'code',
+//     })
+//
+//     return !!match
+//   },
+//
+//   toggleBoldMark(editor) {
+//     const isActive = CustomEditor.isBoldMarkActive(editor)
+//     Transforms.setNodes(
+//       editor,
+//       { bold: isActive ? null : true },
+//       { match: n => Text.isText(n), split: true }
+//     )
+//   },
+//
+//   toggleCodeBlock(editor) {
+//     const isActive = CustomEditor.isCodeBlockActive(editor)
+//     Transforms.setNodes(
+//       editor,
+//       { type: isActive ? null : 'code' },
+//       { match: n => Editor.isBlock(editor, n) }
+//     )
+//   },
+// }
+//
+//
+// const TextEditor = () => {
+//   const editor = useMemo(() => withReact(createEditor()), [])
+//   // Add the initial value when setting up our state.
+//   const [value, setValue] = useState([
+//     {
+//       type: 'paragraph',
+//       children: [{ text: 'A line of text in a paragraph.' }],
+//     },
+//   ])
+//
+//   useEffect(() => {
+//     if (!value) {
+//        //Transforms.deselect(editor);
+//        // or set selection to neutral state:
+//        editor.selection = { anchor: { path: [0,0], offset:0 }, focus: { path: [0,0], offset: 0 } }
+//        // Transforms.select(editor, {
+//        //   anchor: { path: [0, 0], offset: 0 },
+//        //   focus: { path: [0, 0], offset: 0 },
+//        // })
+//
+//     }
+//   }, [value]);
+//
+  // //FOR RENDERING CUSTOM BLOCKS
+  // const renderElement = useCallback(props => {
+  //   switch (props.element.type) {
+  //     case 'code':
+  //       return <CodeBlock {...props} />
+  //     default:
+  //       return <DefaultElement {...props} />
+  //   }
+  // }, [])
+//
+//   //FOR RENDERING CUSTOM LEAVES
+//   const renderLeaf = useCallback(props => {
+//     return <Leaf {...props} />
+//   }, [])
+//
+//   const DefaultElement = props => {
+//     return <p {...props.attributes}>{props.children}</p>
+//   }
+//
+//   return (
+//     <Slate
+//       editor={editor}
+//       value={value}
+//       onChange={newValue => setValue(newValue)}
+//
+//       style={{border: 'solid', width: '100%', height: '100%'}}
+//     >
+//     <Editable
+//       renderElement={renderElement}
+//       renderLeaf={renderLeaf}
+//
+//       onKeyDown={event => {
+//           let commandKey = event.ctrlKey || event.metaKey ? true : false
+//           if (!commandKey) {
+//             return
+//           } else {
+//             console.log("command issued " + event.key)
+//             switch (event.key) {
+//               case '`': {
+//                 event.preventDefault()
+//                 CustomEditor.toggleCodeBlock(editor)
+//                 break
+//               }
+//
+//               case 'b': {
+//                 event.preventDefault()
+//                 CustomEditor.toggleBoldMark(editor)
+//                 break
+//               }
+//             }
+//           }
+//         }}
+//
+//     />
+//     </Slate>
+//   )
+// }
 
 export default TextEditor
